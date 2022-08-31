08f07fed823fbf4201cba560b444f23151ab49f4
adenprince <45126168+adenprince@users.noreply.github.com>
2022-08-30 19:15:25 -0500

    Remove recipe book from grinder block GUI

--- src/main/java/net/nightfallclosure/stonegrinder/screen/GrinderScreen.java
+++ src/main/java/net/nightfallclosure/stonegrinder/screen/GrinderScreen.java
@@ -10,7 +10,15 @@ public class GrinderScreen extends AbstractFurnaceScreen<GrinderScreenHandler> {
     public static final Identifier TEXTURE = new Identifier("textures/gui/container/blast_furnace.png");
 
     public GrinderScreen(GrinderScreenHandler handler, PlayerInventory inventory, Text title) {
-        // TODO: Fix using blast furnace recipe book screen
         super(handler, new BlastFurnaceRecipeBookScreen(), inventory, title, TEXTURE);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+
+        // This call is to hide the recipe book icon
+        // It might be better to use a mixin
+        this.clearChildren();
     }
 }
