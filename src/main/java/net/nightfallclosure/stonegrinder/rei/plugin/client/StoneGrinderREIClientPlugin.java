baefc32f8dffb582a37c8cc2dc1827e3f0c48c04
adenprince <45126168+adenprince@users.noreply.github.com>
2022-09-10 20:18:55 -0500

    Add drill to grinding recipe Roughly Enough Items mod GUI

--- src/main/java/net/nightfallclosure/stonegrinder/rei/plugin/client/StoneGrinderREIClientPlugin.java
+++ src/main/java/net/nightfallclosure/stonegrinder/rei/plugin/client/StoneGrinderREIClientPlugin.java
@@ -4,10 +4,10 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
-import me.shedaniel.rei.plugin.client.categories.cooking.DefaultCookingCategory;
 import net.nightfallclosure.stonegrinder.recipe.GrindingRecipe;
 import net.nightfallclosure.stonegrinder.registry.ModBlocks;
 import net.nightfallclosure.stonegrinder.registry.ModRecipes;
+import net.nightfallclosure.stonegrinder.rei.plugin.client.categories.DefaultGrindingCategory;
 import net.nightfallclosure.stonegrinder.rei.plugin.common.displays.cooking.DefaultGrindingDisplay;
 
 import static net.nightfallclosure.stonegrinder.rei.plugin.common.StoneGrinderREIPlugin.GRINDING;
@@ -15,7 +15,7 @@ import static net.nightfallclosure.stonegrinder.rei.plugin.common.StoneGrinderRE
 public class StoneGrinderREIClientPlugin implements REIClientPlugin {
     @Override
     public void registerCategories(CategoryRegistry registry) {
-        registry.add(new DefaultCookingCategory(GRINDING, EntryStacks.of(ModBlocks.GRINDER_BLOCK_ITEM),
+        registry.add(new DefaultGrindingCategory(GRINDING, EntryStacks.of(ModBlocks.GRINDER_BLOCK_ITEM),
                 "category.stonegrinder.grinding"));
         registry.addWorkstations(GRINDING, EntryStacks.of(ModBlocks.GRINDER_BLOCK_ITEM));
     }
---
+++ src/main/java/net/nightfallclosure/stonegrinder/rei/plugin/client/categories/DefaultGrindingCategory.java
@@ -0,0 +1,58 @@
+package net.nightfallclosure.stonegrinder.rei.plugin.client.categories;
+
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.gui.widgets.BurningFire;
+import me.shedaniel.rei.api.client.gui.widgets.Slot;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.entry.EntryStack;
+import me.shedaniel.rei.plugin.client.categories.cooking.DefaultCookingCategory;
+import me.shedaniel.rei.plugin.common.displays.cooking.DefaultCookingDisplay;
+import net.nightfallclosure.stonegrinder.screen.GrinderScreen;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class DefaultGrindingCategory extends DefaultCookingCategory {
+    public DefaultGrindingCategory(CategoryIdentifier<? extends DefaultCookingDisplay> identifier, EntryStack<?> logo,
+                                   String categoryName) {
+        super(identifier, logo, categoryName);
+    }
+
+    @Override
+    public List<Widget> setupDisplay(DefaultCookingDisplay display, Rectangle bounds) {
+        List<Widget> defaultCookingCategoryWidgets = super.setupDisplay(display, bounds);
+
+        Stream<Slot> defaultCookingCategorySlots = defaultCookingCategoryWidgets
+                .stream()
+                .filter(x -> x instanceof Slot)
+                .map(x -> (Slot)x);
+
+        // Move input slot down
+        Optional<Slot> cookingInputSlot = defaultCookingCategorySlots
+                .min(Comparator.comparingInt(x -> x.getBounds().getX()));
+        cookingInputSlot.ifPresent(slot -> slot.getBounds().y += 13);
+
+        // Move burning fire up and left
+        Optional<BurningFire> defaultCookingCategoryBurningFire = defaultCookingCategoryWidgets
+                .stream()
+                .filter(x -> x instanceof BurningFire)
+                .map(x -> (BurningFire)x)
+                .findFirst();
+        defaultCookingCategoryBurningFire.ifPresent(burningFire -> {
+            burningFire.getBounds().x -= 18;
+            burningFire.getBounds().y -= 6;
+        });
+
+        cookingInputSlot.ifPresent(slot -> {
+            Widget drillWidget = Widgets.createTexturedWidget(GrinderScreen.TEXTURE,
+                    slot.getBounds().getX() + 2, slot.getBounds().getY() - 16, 57, 36, 14, 14);
+            defaultCookingCategoryWidgets.add(drillWidget);
+        });
+
+        return defaultCookingCategoryWidgets;
+    }
+}
