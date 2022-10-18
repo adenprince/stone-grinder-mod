4b988aa075617006df3bcbb18e3de5746e811d3c
adenprince <45126168+adenprince@users.noreply.github.com>
2022-10-17 21:12:59 -0500

    Simplify grinder block entity registration in Fabric version

--- fabric/src/main/java/net/nightfallclosure/stonegrinder/registry/ModBlockEntities.java
+++ fabric/src/main/java/net/nightfallclosure/stonegrinder/registry/ModBlockEntities.java
@@ -1,24 +1,21 @@
 package net.nightfallclosure.stonegrinder.registry;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.nightfallclosure.stonegrinder.StoneGrinderMod;
 import net.nightfallclosure.stonegrinder.block.entity.GrinderBlockEntity;
 
+import static net.nightfallclosure.stonegrinder.registry.ModBlocks.GRINDER_BLOCK;
+
 public class ModBlockEntities {
     public static BlockEntityType<GrinderBlockEntity> GRINDER_BLOCK_ENTITY_TYPE;
 
-    private static BlockEntityType<GrinderBlockEntity> registerBlockEntity(String name, Block block) {
-        return Registry.register(Registry.BLOCK_ENTITY_TYPE,
-                new Identifier(StoneGrinderMod.MOD_ID, name),
-                FabricBlockEntityTypeBuilder.create(GrinderBlockEntity::new,
-                        block).build(null));
-    }
-
     public static void registerModBlockEntities() {
-        GRINDER_BLOCK_ENTITY_TYPE = registerBlockEntity("grinder", ModBlocks.GRINDER_BLOCK);
+        GRINDER_BLOCK_ENTITY_TYPE = Registry.register(Registry.BLOCK_ENTITY_TYPE,
+                new Identifier(StoneGrinderMod.MOD_ID, "grinder"),
+                FabricBlockEntityTypeBuilder.create(GrinderBlockEntity::new,
+                        GRINDER_BLOCK).build(null));
     }
 }
