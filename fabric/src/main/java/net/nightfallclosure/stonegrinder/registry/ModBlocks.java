fa96067ef918ad264617e3b886d99670b769d9a6
adenprince <45126168+adenprince@users.noreply.github.com>
2022-10-17 20:36:39 -0500

    Simplify grinder block and item registration in Fabric version

--- fabric/src/main/java/net/nightfallclosure/stonegrinder/registry/ModBlocks.java
+++ fabric/src/main/java/net/nightfallclosure/stonegrinder/registry/ModBlocks.java
@@ -9,38 +9,21 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Pair;
 import net.minecraft.util.registry.Registry;
 import net.nightfallclosure.stonegrinder.StoneGrinderMod;
 import net.nightfallclosure.stonegrinder.block.custom.GrinderBlock;
 
 public class ModBlocks {
-    public static Item GRINDER_BLOCK_ITEM;
-    public static Block GRINDER_BLOCK;
-
-    private static Pair<Item, Block> registerBlock(String name, Block block, ItemGroup tab) {
-        Item registeredItem = registerBlockItem(name, block, tab);
-        Block registeredBlock = Registry.register(Registry.BLOCK,
-                new Identifier(StoneGrinderMod.MOD_ID, name), block);
-        return new Pair<>(registeredItem, registeredBlock);
-    }
-
-    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
-        return Registry.register(Registry.ITEM, new Identifier(StoneGrinderMod.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings().group(tab)));
-    }
+    public static final Block GRINDER_BLOCK = new GrinderBlock(FabricBlockSettings
+            .of(Material.STONE)
+            .strength(3.5f, 17.5f)
+            .requiresTool()
+            .sounds(BlockSoundGroup.STONE));
+    public static final Item GRINDER_BLOCK_ITEM = new BlockItem(GRINDER_BLOCK,
+            new FabricItemSettings().group(ItemGroup.DECORATIONS));
 
     public static void registerModBlocks() {
-        StoneGrinderMod.LOGGER.debug("Registering mod blocks for " + StoneGrinderMod.MOD_ID);
-        Block unregisteredGrinderBlock = new GrinderBlock(FabricBlockSettings
-                .of(Material.STONE)
-                .strength(3.5f, 17.5f)
-                .requiresTool()
-                .sounds(BlockSoundGroup.STONE));
-
-        Pair<Item, Block> registeredItemBlockPair =
-                registerBlock("grinder", unregisteredGrinderBlock, ItemGroup.DECORATIONS);
-        GRINDER_BLOCK_ITEM = registeredItemBlockPair.getLeft();
-        GRINDER_BLOCK = registeredItemBlockPair.getRight();
+        Registry.register(Registry.BLOCK, new Identifier(StoneGrinderMod.MOD_ID, "grinder"), GRINDER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(StoneGrinderMod.MOD_ID, "grinder"), GRINDER_BLOCK_ITEM);
     }
 }
